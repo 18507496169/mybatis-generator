3a5650fc51c2f53db35a62b1cc4af2d15ba098a4
huangch <huangchaohui@duiba.com.cn>
2019-08-20 15:59:45 +0800

    mybatis数据库配置修改

--- src/test/java/com/xx/mybatis/generator/CodeGenerator.java
+++ src/test/java/com/xx/mybatis/generator/CodeGenerator.java
@@ -6,19 +6,24 @@ public class CodeGenerator {
 
     public static void main(String[] args) {
 
-        String DB_URL = "jdbc:mysql://172.16.18.21:3306/tuia_media?createDatabaseIfNotExist=true&useUnicode=true&characterEncoding=UTF-8&zeroDateTimeBehavior=convertToNull&allowMultiQueries=true&connectTimeout=5000&socketTimeout=60000";
-        String user = "superman";
-        String pwd = "3xBasjDl@iJA3T@$c-u3zLZvZWia49a4";
+        String[] tableList = {"xm_banner_config"};
 
-        GeneratorConfig config = new GeneratorConfig(DB_URL, user, pwd);
-        config.setDaoPackage("com.xx.mybatis.generator.mapper");
-        config.setDOpackage("com.xx.mybatis.generator.domain");
-        config.setXmlLocation("src/main/resources/");
-        config.setTableCatalog("tuia_media");
-        config.setTableName("tb_activity");
-        config.setTablePrefix("tb");
-        config.setModuleName("");
+        for (String tableName : tableList) {
+            String DB_URL = "jdbc:mysql://172.16.18.21:3306/xiaoman_saas?createDatabaseIfNotExist=true&useUnicode=true&characterEncoding=UTF-8&zeroDateTimeBehavior=convertToNull&allowMultiQueries=true&connectTimeout=5000&socketTimeout=60000";
+            String user = "superman";
+            String pwd = "3xBasjDl@iJA3T@$c-u3zLZvZWia49a4";
 
-        MybatisGenerator.genCode(config);
+            GeneratorConfig config = new GeneratorConfig(DB_URL, user, pwd);
+            config.setDaoPackage("com.xx.mybatis.generator.mapper");
+            config.setDOpackage("com.xx.mybatis.generator.domain");
+            config.setXmlLocation("src/main/resources/");
+            config.setTableCatalog("xiaoman_saas");
+            config.setTableName(tableName);
+            config.setTablePrefix("xm");
+            config.setModuleName("");
+
+            MybatisGenerator.genCode(config);
+        }
+
     }
 }
